b6eb20611bdd01dad99b03d8cb8fdd85c07566c7
Ryan Romano <ryanaromano@gmail.com>
2018-11-27 15:29:54 -0500

    New route to /new-session, link from all sessions page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
 import HomePage from './pages/HomePage';
 import AllSessionsPage from './pages/AllSessionsPage';
+import NewSessionPage from './pages/NewSessionPage';
 
 const routing = (
     <Router>
@@ -10,6 +12,7 @@ const routing = (
             <Switch>
                 <Route exact path="/" component={HomePage}/>
                 <Route path="/sessions" component={AllSessionsPage}/>
+                <Route path="/new-session" component={NewSession}/>
             </Switch>
         </div>
     </Router>
