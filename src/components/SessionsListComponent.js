68b940515c2d44e916c5dc5f9d7adee854187078
Ryan Romano <ryanaromano@gmail.com>
2018-11-27 11:43:46 -0500

    Separate logic btwn page render/components

---
+++ src/components/SessionsListComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class SessionsListComponent extends React.Component {
+
+    render() {
+        return(
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import Home from './components/Home';
+import HomePage from './pages/HomePage';
+import SessionsPage from './pages/SessionsPage';
 
 const routing = (
     <Router>
         <div>
             <Switch>
-                <Route exact path="/" component={Home}/>
+                <Route exact path="/" component={HomePage}/>
+                <Route path="/sessions" component={SessionsPage}/>
             </Switch>
         </div>
     </Router>
