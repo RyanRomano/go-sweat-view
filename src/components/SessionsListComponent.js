dc573b3a5e4622e472ce83bb2ddf7c6ef0258cf9
Ryan Romano <ryanaromano@gmail.com>
2018-11-27 12:39:55 -0500

    Sessions data displaying to /sessions

--- src/components/SessionsListComponent.js
+++ src/components/SessionsListComponent.js
@@ -2,13 +2,22 @@ import React from 'react';
 
 export default class SessionsListComponent extends React.Component {
 
+// TODO: CREATE SINGLE SESSION COMPONENT AND PASS THIS DATA :) -> 
+// RENDER INSIDE THAT INSTEAD OF DIV P P P 
+
     render() {
         return(
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <div>
+                {this.props.sessions.map(function (session) {
+                    return (
+                        <div key={session.id}>
+                            <p>{session.id}</p>
+                            <p>{session.date}</p>
+                            <p>{session.muscles_worked}</p>
+                        </div>
+                    )
+                })}
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import SessionsPage from './pages/SessionsPage';
+import AllSessionsPage from './pages/AllSessionsPage';
 
 const routing = (
     <Router>
         <div>
             <Switch>
                 <Route exact path="/" component={HomePage}/>
-                <Route path="/sessions" component={SessionsPage}/>
+                <Route path="/sessions" component={AllSessionsPage}/>
             </Switch>
         </div>
     </Router>
