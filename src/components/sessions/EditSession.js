6762f1aa9a062639687bd7295029521387ea19e7
Ryan Romano <ryanaromano@gmail.com>
2018-12-01 13:16:35 -0500

    Functional edit session component

--- src/components/sessions/EditSession.js
+++ src/components/sessions/EditSession.js
@@ -2,11 +2,38 @@ import React from 'react';
 
 export default class EditSession extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            muscles_worked: this.props.location.state
+        };
+    }
+    
+    handleChange = (event) => {
+        this.setState({muscles_worked: event.target.value});
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        fetch(`http://localhost:3000/sessions/${this.props.match.params.id}`, {
+            method: 'put',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(this.state)
+        }).then(window.location.href = `http://localhost:1234/sessions/`)
+        .catch(error => console.error(error));
+    }
+
     render() {
         return (
             <div>
-                <p>Hello world! Edit Session # {this.props.match.params.id} here.</p>
-            </div>
+            <h2>Edit Session id:{this.props.match.params.id}</h2>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    <input type="text" name="muscles_worked" defaultValue={this.state.muscles_worked} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
         )
     }
 }
--- src/components/sessions/SessionListItem.js
+++ src/components/sessions/SessionListItem.js
@@ -10,7 +10,7 @@ export default class SessionListItem extends React.Component {
                 <p>Session Date: {session.date}</p>
                 <p>Muscles Worked: {session.muscles_worked}</p>
                 <button onClick={() => this.props.viewHandler(session.id)}>View Workouts</button>
-                <button onClick={() => this.props.editHandler(session.id)}>Edit</button>
+                <button onClick={() => this.props.editHandler(session.id, session.muscles_worked)}>Edit</button>
                 <button onClick={() => this.props.deleteHandler(session.id)}>Delete</button>
                 <hr/>
             </div>
