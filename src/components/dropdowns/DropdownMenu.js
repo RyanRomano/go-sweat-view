8c784b2aad9d54331216d5982324b3c7a0c156ca
Ryan Romano <ryanaromano@gmail.com>
2018-12-07 16:24:55 -0500

    Organized dropdown exercises by musclegroupid

--- src/components/dropdowns/DropdownMenu.js
+++ src/components/dropdowns/DropdownMenu.js
@@ -1,28 +1,48 @@
 import React from 'react';
 
 export default class DropdownMenu extends React.Component {
-
     render() {
+        let muscleIDarray = [];
+
+        let renderEquipmentOptions = this.props.dropDownItems.map(item => {
+            return (
+                <option key={item.id} value={item.id}>{item.equipment_type}</option>
+            )
+        });
+
+        let renderExerciseOptions = this.props.dropDownItems.map((group, muscle_group_index) => {
+            if(!muscleIDarray.includes(group.muscle_group_id)) {
+                muscleIDarray.push(group.muscle_group_id);
+                return (
+                    <optgroup key={group.muscle_group_id} label={group.muscle_group}>
+                        {group.muscle_group_id}
+                        {this.props.dropDownItems.map(exercise => {
+                            if(exercise.muscle_group_id == (muscle_group_index + 1)){
+                                return (
+                                    <option key={exercise.id} value={exercise.id}>{exercise.exercise_name}</option>
+                                )
+                            }
+                        })}
+                    </optgroup>
+                );
+        }});
+
+        let renderOptions;
+
+        if(this.props.identifier == 0){
+            renderOptions = renderExerciseOptions;
+        } else {
+            renderOptions = renderEquipmentOptions;
+        }
+
         return (
-            <div className="dd-wrapper">
-                <select 
-                    value={this.props.selectedValue} 
-                    onChange={(event) => this.props.handleDropDownChange(this.props.identifier, event)}
-                >
-                    <option value={-1}>-- Select --</option>
-                    {this.props.dropDownItems.map(item => {
-                        if(this.props.identifier == 0) {
-                            return (
-                                <option key={item.id} value={item.id}>{item.exercise_name}</option>
-                            )
-                        } else {
-                            return (
-                                <option key={item.id} value={item.id}>{item.equipment_type}</option>
-                            )
-                        }
-                    })}
-                </select>
-            </div>
+            <select 
+                value={this.props.selectedValue} 
+                onChange={(event) => this.props.handleDropDownChange(this.props.identifier, event)}
+            >
+                <option value={-1}>-- Select --</option>
+                {renderOptions}
+            </select>
         )
     }
 }
