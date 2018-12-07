f18f9001d752339893f12eb4a0cce0d4cd2421a6
Ryan Romano <ryanaromano@gmail.com>
2018-12-07 12:29:25 -0500

    Dropdown list updates NewWorkout state

--- src/components/workouts/NewWorkout.js
+++ src/components/workouts/NewWorkout.js
@@ -7,41 +7,112 @@ export default class NewWorkout extends React.Component {
         super(props);
         this.state = {
             session_id: this.props.match.params.id,
-            exercise_id: null,
-            equipment_id: null,
+            exercise_id: -1,
+            equipment_id: -1,
             sets: null,
             reps: null,
             set1: null,
             set2: null,
             set3: null,
-            notes: ''
+            notes: '',
+            exercises: [],
+            equipment: []
         };
+        this.exerciseDropDownIdentifier = 0;
+        this.equipmentDropDownIdentifier = 1;
+
+    }
+
+    componentDidMount(){
+        this.fetchExercises();
+        this.fetchEquipment();
+    }
+
+    fetchExercises = () => {
+        fetch('http://localhost:3000/exercises')
+        .then(response => response.json())
+        .then(json => {
+            json.forEach(exercise => {
+                this.setState({ exercises: [...this.state.exercises, exercise] });
+            });
+        })
+        .catch(error => console.error(error));
+    }
+
+    fetchEquipment = () => {
+        fetch('http://localhost:3000/exercises')
+        .then(response => response.json())
+        .then(json => {
+            json.forEach(equipment => {
+                this.setState({ equipment: [...this.state.equipment, equipment] });
+            });
+        })
+        .catch(error => console.error(error));
+    }
+
+    handleDropDownChange = (id, event) => {
+        if(id == 0) {
+            this.setState({exercise_id: event.target.value});
+        } else {
+            this.setState({equipment_id: event.target.value});
+        }
     }
     
-    handleChange = (event) => {
+    handleTextFieldChange = (event) => {
         this.setState({ [event.target.name]: event.target.value });
     }
     
     handleSubmit = (e) => {
         e.preventDefault();
-        fetch(`http://localhost:3000/workouts/`, {
-            method: 'post',
-            headers: {'Content-Type':'application/json'},
-            body: JSON.stringify(this.state)
-        })
-        .then(window.location.href = `http://localhost:1234/sessions/${this.state.session_id}`)
-        .catch(error => console.error(error)); 
+
+        const newWorkout = {
+            session_id: this.state.session_id,
+            exercise_id: this.state.exercise_id,
+            equipment_id: this.state.equipment_id,
+            sets: this.state.sets,
+            reps: this.state.reps,
+            set1: this.state.set1,
+            set2: this.state.set2,
+            set3: this.state.set3,
+            notes: this.state.notes,
+        }
+       
+        if(newWorkout.exercise_id == -1 && newWorkout.equipment_id == -1) {
+            alert('Please select an exercise and type of equipment used!');
+        } else if(newWorkout.exercise_id == -1) {
+            alert('Please select an exercise!');
+        } else if(newWorkout.equipment_id == -1) {
+            alert('Please select the type of equipment used!');
+        } else {
+            // fetch(`http://localhost:3000/workouts/`, {
+            //     method: 'post',
+            //     headers: {'Content-Type':'application/json'},
+            //     body: JSON.stringify(this.state)
+            // })
+            // .then(window.location.href = `http://localhost:1234/sessions/${this.state.session_id}`)
+            // .catch(error => console.error(error)); 
+            alert(newWorkout);
+        }
     }
-    
+
     render() {
         return (
             <div>            
                 <h2>New Workout</h2>
-                <WorkoutForm handleSubmit={this.handleSubmit} 
-                             handleChange={this.handleChange} 
-                             workoutData={this.state}
+                <WorkoutForm
+        
+                    handleSubmit={this.handleSubmit} 
+                    handleTextFieldChange={this.handleTextFieldChange} 
+                    handleDropDownChange={this.handleDropDownChange}
+
+                    workoutData={this.state}
+
+                    exerciseDropDownIdentifier={this.exerciseDropDownIdentifier}
+                    equipmentDropDownIdentifier={this.equipmentDropDownIdentifier}
+
                 />
             </div>
         )
     }
 }
+              
