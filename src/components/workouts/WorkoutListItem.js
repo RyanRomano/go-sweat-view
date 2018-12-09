eee9c788b6371b385598fd82c3828744416f9993
Ryan Romano <ryanaromano@gmail.com>
2018-12-09 11:19:16 -0500

    Edit form functions standalone

--- src/components/workouts/WorkoutListItem.js
+++ src/components/workouts/WorkoutListItem.js
@@ -2,21 +2,8 @@ import React from 'react';
 
 export default class WorkoutListItem extends React.Component {
 
-
-
     render() {
         let workout = this.props.workoutData;
-
-        let currentWorkoutData = {
-            exercise_id: workout.exercise_id,
-            equipment_id: workout.equipment_id,
-            sets: workout.sets,
-            reps: workout.reps,
-            set1: workout.set1,
-            set2: workout.set2,
-            set3: workout.set3,
-            notes: workout.notes
-        }
 
         return (
             <div key={workout.id}>
@@ -28,7 +15,7 @@ export default class WorkoutListItem extends React.Component {
                 <p>Set 2: {workout.set2}</p>
                 <p>Set 3: {workout.set3}</p>
                 <p>Notes: {workout.notes}</p>
-                <button onClick={() => this.props.editHandler(workout.id, currentWorkoutData)}>Edit</button>
+                <button onClick={() => this.props.editHandler(workout.id)}>Edit</button>
                 <button onClick={() => this.props.deleteHandler(workout.id)}>Delete</button>
                 <hr/>
             </div>        
